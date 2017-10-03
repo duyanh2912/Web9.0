0693eabd63c852e03d9143f134fd6e6f0ce86765
Duy Anh <duyanh2912@gmail.com>
2017-10-03 22:21:07 +0700

    Configured WebStorm

--- Practice/Practice1/searchPractice.js
+++ Practice/Practice1/searchPractice.js
@@ -1,7 +1,7 @@
-'use strict'
+'use strict';
 
 function search(input, target) {
   return input.indexOf(target);  // Remove this line and change to your own algorithm
 }
 
-module.exports = search
+module.exports = search;

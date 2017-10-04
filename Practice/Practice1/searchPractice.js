5f984563208abff4cfc635a8b25d2dcc522e1167
Duy Anh <duyanh2912@gmail.com>
2017-10-04 15:40:37 +0700

    Done Practice 1 & 2

--- Practice/Practice1/searchPractice.js
+++ Practice/Practice1/searchPractice.js
@@ -1,7 +1,27 @@
 'use strict';
+const sort = require("../Practice2/sortPractice");
 
 function search(input, target) {
-  return input.indexOf(target);  // Remove this line and change to your own algorithm
+    function helper(input, target, startIndex, endIndex) {
+        if (startIndex === endIndex && input[startIndex] !== target) {
+            return -1;
+        }
+
+        const middleIndex = Math.floor((endIndex + startIndex) / 2);
+        const middleValue = input[middleIndex];
+
+        if (middleValue === target) {
+            return middleIndex;
+        }
+        else if (target < middleValue) {
+            return helper(input, target, startIndex, middleIndex - 1);
+        }
+        else if (target > middleValue) {
+            return helper(input, target, middleIndex + 1, endIndex)
+        }
+    }
+
+    return helper(sort(input), target, 0, input.length - 1);
 }
 
 module.exports = search;
